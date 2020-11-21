6869d411368e73a55a7c181bce2fc1b1ab9353c2
exoushia <45519518+exoushia@users.noreply.github.com>
2020-11-21 12:52:28 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,9 +3,10 @@ import torch
 from utils import *
 from prepro.data_loading import *
 from prepro.reading_files import *
-from network_architecture import model_bert
+from network_architecture import *
 from settings import * 
-
+from test import *
+import torch.optim as optim
 
 
 def run_preprocess(df,filename,cutoff,preprocessed_filepath):
@@ -88,4 +89,55 @@ def train_model_bert(train_loader,validation_dataloader,config,total_steps,path_
 	return loss_values , val_accuracy, model 
 
 
+
+def train_model_custom_bert(mode,train_loader,validation_dataloader,config,total_steps,_):
+
+	# Store the average loss after each epoch so we can plot them.
+	model_name = config.model_name
+	num_labels = config.num_labels
+	loss_values = []
+	val_accuracy = []
+
+	print("Loading model ...")
+
+	if mode == 'lstm':
+		model = CustomBERTModel(model_name,num_labels,config,total_steps)
+	else :
+		model = CustomBERTModel_hidden(model_name,num_labels,config,total_steps)
+
+	model.to(torch.device(device)) 
+
+	optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
+
+	start_of_training = time.time()
+
+	# For each epoch...
+	for epoch_i in range(0, config.epochs):
+		# ========================================
+		#               Training
+		# ========================================
+		model.train()
+		optimizer.zero_grad()
+		training_loss = model(epoch_i,train_loader)
+		loss_values.append(training_loss)
+		optimizer.step()
+
+		if epoch_i%1==0:
+			# ========================================
+			#               Validation
+			# ========================================
+			model.eval()
+			if mode=='lstm':
+				eval_acc = evaluate(model,validation_dataloader)
+			else : 
+				eval_acc = evaluate2(model,validation_dataloader)
+
+			val_accuracy.append(eval_acc)
+
+	print("\n")
+	elapsed = format_time(time.time() - start_of_training)
+	print("Training complete!")
+	print("Total Training time: {}".format(elapsed))
+
+	return loss_values , val_accuracy, model 
 
